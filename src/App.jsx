7c368ece7082fcd1b001cc0630a5a0d1f8534b30
Md Raza <mdraza8397@gmail.com>
2026-01-14 23:27:44 +0530

    react component done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./header.jsx";
+import Login, {Profile, Setting, user_id} from "./UserComponent.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,10 @@ function App() {
     <>
       <Header />
       <Name />
+      <Login />
+      <Profile />
+      <Setting />
+      <h1>{user_id}</h1>
     </>
   );
 }
