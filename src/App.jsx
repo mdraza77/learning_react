3c861a07c5db3c0f955f9655b733f4cac9fdb5c8
Md Raza <mdraza8397@gmail.com>
2026-01-15 23:15:36 +0530

    react component hide and show

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import "./App.css";
 import Header from "./header.jsx";
 import Login, { Profile, Setting, user_id } from "./UserComponent.jsx";
 import Counter from "./Counter.jsx";
+import User from "./User.jsx";
 
 function App() {
   const [fruit, setFruit] = useState("Apple");
   const handleFruit = () => {
     setFruit("Banana");
   };
+  const [display, setDisplay] = useState(true);
   return (
     <>
       <p>{fruit}</p>
       <button onClick={handleFruit}>Change Fruit Name</button>
       <Counter />
+      {/* {display ? <p>Hi, This is Md Raza</p> : null} */}
+      {/* {display ? <p>Hi, This is Md Raza</p> : <p>Not Available</p>} */}
+      {display ? <User /> : <p>Not Available</p>}
+      <button onClick={() => setDisplay(!display)}>Toggle Display</button>
     </>
   );
 }
