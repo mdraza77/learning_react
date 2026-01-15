fe63e863e9759292393bbe590faaa4cb710125fa
Md Raza <mdraza8397@gmail.com>
2026-01-15 22:54:20 +0530

    react state

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./header.jsx";
 import Login, { Profile, Setting, user_id } from "./UserComponent.jsx";
+import Counter from "./Counter.jsx";
 
 function App() {
-  const myName = "Md Raza";
-  let myAge = 20;
-
-  let profile_img = "public\\profile-img.png";
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  const fruit = (name) => {
-    alert(name);
+  const [fruit, setFruit] = useState("Apple");
+  const handleFruit = () => {
+    setFruit("Banana");
   };
-
   return (
     <>
-      <Header />
-      <p>
-        My name is {myName} and age is {myAge}
-      </p>
-      <p>Sum of a and b is {sum(10, 20)}</p>
-      <input type="text" value={myName} />
-      <img className="profile-image" src={profile_img} alt="Profile" />
-      <button onClick={() => fruit("Apple")}>Apple</button>
-      <button onClick={() => fruit("Banana")}>Banana</button>
+      <p>{fruit}</p>
+      <button onClick={handleFruit}>Change Fruit Name</button>
+      <Counter />
     </>
   );
 }
