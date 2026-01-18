31226b518b764cb30d3b387d6ea0bc01df9de336
Md Raza <mdraza8397@gmail.com>
2026-01-18 22:01:36 +0530

    tailwind installed

--- src/User.jsx
+++ src/User.jsx
@@ -1,7 +1,7 @@
 const User = ({name, age, email}) => {
   return (
     <div>
-      <p className="text-red">Name is {name}</p>
+      <p className="text-red-500">Name is {name}</p>
       <p>Age is {age}</p>
       <p>Email is {email}</p>
     </div>
