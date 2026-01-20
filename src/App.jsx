e6087bb80ce29cfd13075a2b55544743bb73b8b9
Md Raza <mdraza8397@gmail.com>
2026-01-20 17:38:29 +0530

    Controlled Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,57 @@ import "./App.css";
 import User from "./User.jsx";
 
 function App() {
-  const [student, setStudent] = useState("Md Raza");
-  const [input, setInput] = useState();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="p-4 bg-blue-500">
-        <input
-          onChange={(event) => setInput(event.target.value)}
-          value={input}
-          className="py-2 px-4 border rounded outline-none"
-          type="text"
-          placeholder="Enter Name"
-        />
-        <p className="text-white">{input}</p>
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setInput("")}
-        >
-          Clear
-        </button>
+      <div className="p-4 bg-blue-500 rounded">
+        <div></div>
+        <form action="" method="get">
+          <input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            className="py-2 px-4 border rounded outline-none"
+            type="text"
+            placeholder="Enter Name"
+          />
+          <p className="text-white">{name}</p>
+          <br /> <br />
+          <input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className="py-2 px-4 border rounded outline-none"
+            type="email"
+            placeholder="Enter Email"
+          />
+          <p className="text-white">{email}</p>
+          <br /> <br />
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="py-2 px-4 border rounded outline-none"
+            type="password"
+            placeholder="Enter Password"
+          />
+          <p className="text-white">{password}</p>
+          <br /> <br />
+          <div className="flex gap-2">
+            <button className="px-4 py-2 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400">
+              Submit
+            </button>
+            <button
+              className="px-4 py-2 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+              onClick={() => {
+                setName("");
+                setEmail("");
+                setPassword("");
+              }}
+            >
+              Clear
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
